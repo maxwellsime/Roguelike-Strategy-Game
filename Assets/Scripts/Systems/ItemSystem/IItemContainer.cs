e63d2a6b4c87093087905ab48393de4416ffccab
Maxwell Sime <maxwellsime@gmail.com>
2025-01-05 14:34:11 +0000

    Clean up folder structure, removing redundant files

--- Assets/Scripts/Systems/ItemSystem/IItemContainer.cs
+++
@@ -1,7 +0,0 @@
-interface IItemContainer {
-    Item AddItem(Item item);
-    void RemoveItem(Item item);
-    void RemoveAt(int index);
-    void Swap(int indexOne, int indexTwo);
-    bool HasItem(Item item);
-}
